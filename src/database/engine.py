c74c801b93c01529dad0e180374a181fc962ba65
ebozik <erik23.bozik@gmail.com>
2025-01-20 19:23:01 +0100

    database engine

--- src/database/engine.py
+++ src/database/engine.py
@@ -1,3 +1,7 @@
 from sqlalchemy import URL, create_engine
 
-engine = create_engine(URL.create())
+from ..schemas import DatabaseSettings
+
+engine = create_engine(
+    URL.create(drivername="postgresql+psycopg2", **DatabaseSettings().model_dump())  # type: ignore
+)
