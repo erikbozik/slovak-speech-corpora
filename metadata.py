18d20f7567f1183e6cee0aab88894307058fb1b8
ebozik <erik23.bozik@gmail.com>
2025-01-24 18:26:47 +0100

    place data in common directory and refactor

--- metadata.py
+++ metadata.py
@@ -2,46 +2,46 @@ from src.schemas import DataMetaData
 
 VOXPOPULI_METADATA = [
     DataMetaData(
-        data_path="voxpopuli/asr_train.tsv",
-        audio_dir_path="voxpopuli/train",
+        data_path="data/voxpopuli/asr_train.tsv",
+        audio_dir_path="data/voxpopuli/train",
         source_part="train",
     ),
     DataMetaData(
-        data_path="voxpopuli/asr_dev.tsv",
-        audio_dir_path="voxpopuli/dev",
+        data_path="data/voxpopuli/asr_dev.tsv",
+        audio_dir_path="data/voxpopuli/dev",
         source_part="dev",
     ),
     DataMetaData(
-        data_path="voxpopuli/asr_test.tsv",
-        audio_dir_path="voxpopuli/test",
+        data_path="data/voxpopuli/asr_test.tsv",
+        audio_dir_path="data/voxpopuli/test",
         source_part="test",
     ),
 ]
 
 COMMON_VOICE_METADATA = [
     # DataMetaData(
-    #     data_path="common_voice/invalidated.tsv",
-    #     audio_dir_path="common_voice/clips",
+    #     data_path="data/common_voice/invalidated.tsv",
+    #     audio_dir_path="data/common_voice/clips",
     #     source_part="invalidated",
     # ),
     DataMetaData(
-        data_path="common_voice/validated.tsv",
-        audio_dir_path="common_voice/clips",
+        data_path="data/common_voice/validated.tsv",
+        audio_dir_path="data/common_voice/clips",
         source_part="validated",
     ),
     # DataMetaData(
-    #     data_path="common_voice/dev.tsv",
-    #     audio_dir_path="common_voice/clips",
+    #     data_path="data/common_voice/dev.tsv",
+    #     audio_dir_path="data/common_voice/clips",
     #     source_part="dev",
     # ),
     # DataMetaData(
-    #     data_path="common_voice/test.tsv",
-    #     audio_dir_path="common_voice/clips",
+    #     data_path="data/common_voice/test.tsv",
+    #     audio_dir_path="data/common_voice/clips",
     #     source_part="test",
     # ),
     # DataMetaData(
-    #     data_path="common_voice/train.tsv",
-    #     audio_dir_path="common_voice/clips",
+    #     data_path="data/common_voice/train.tsv",
+    #     audio_dir_path="data/common_voice/clips",
     #     source_part="train",
     # ),
 ]
