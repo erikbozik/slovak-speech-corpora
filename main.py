63e96b74ca02b008d03a7d2352ec9db00f551c71
ebozik <erik23.bozik@gmail.com>
2025-01-24 18:52:03 +0100

    implement fleurs processing

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import structlog
 from sqlalchemy.orm import sessionmaker
 
-from metadata import COMMON_VOICE_METADATA, VOXPOPULI_METADATA, FLEURS_METADATA
+from metadata import COMMON_VOICE_METADATA, FLEURS_METADATA, VOXPOPULI_METADATA
 from src.database import Base, engine
-from src.extractors import CommonVoice, VoxPopuli, Fleurs
+from src.extractors import CommonVoice, Fleurs, VoxPopuli
 
 logger = structlog.get_logger(level="INFO")
 Base.metadata.create_all(bind=engine)
@@ -20,6 +20,12 @@ def insert(extractor):
 
 
 def extract_data():
+    logger.info("Processing Fleurs")
+    for i in FLEURS_METADATA:
+        a = Fleurs(i)
+        logger.info(f"Processing {i.source_part}")
+        insert(a)
+
     logger.info("Processing Common Voice")
     for i in COMMON_VOICE_METADATA:
         a = CommonVoice(i)
@@ -31,10 +37,6 @@ def extract_data():
         a = VoxPopuli(i)
         logger.info(f"Processing {i.source_part}")
         insert(a)
-
-    logger.info("Processing Fleurs")
-    for i in FLEURS_METADATA:
-        a = Fleurs(i)
 
 
 extract_data()
